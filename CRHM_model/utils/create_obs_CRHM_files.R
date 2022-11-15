6d4c3781494440c0f535f1d44077e5b928427d67
Francisco Jara <fcojara506@gmail.com>
2022-11-15 18:06:38 -0300

    interpolated obs data using spline

--- CRHM_model/utils/create_obs_CRHM_files.R
+++ CRHM_model/utils/create_obs_CRHM_files.R
@@ -3,6 +3,7 @@ require(CRHMr)
 require(lubridate)
 require(glue)
 
+
 rep.col<-function(x,n){matrix(rep(x,each=n), ncol=n, byrow=TRUE)}
 
 create_obs_file_per_variable <- function(meteo_CRHM_variable = "t",
@@ -30,15 +31,26 @@ create_obs_file_per_variable <- function(meteo_CRHM_variable = "t",
     timestep = 1,
     variables = meteo_CRHM_variable, #c('t','p','rh','u'),
     reps = ncol(obs_matrix)-1,
-    timezone = 'etc/GMT-4'
+    timezone = 'etc/GMT+4'
   )
 
  df = merge(x = obs_crhm,
             y = obs_matrix,
-            by="datetime")
+            by="datetime",
+            all.x = T)
  
  #remove na columns
  df = df[,colSums(is.na(df)) < nrow(df)]
+ 
+ # interpolate na values
+ df = CRHMr::interpolate(obs = df,
+                         varcols = seq(1,ncol(df)-1),
+                         methods = "spline",
+                         maxlength = 24*5,
+                         quiet = T,
+                         logfile = "")
+ 
+ #define column names
  names(df) = names(obs_crhm)
  
  # format dates to CRHM
@@ -61,24 +73,25 @@ CRHM_var_OBS_col = list(
   "t" = c("temperatura_aire_celsius"),
   "p" = c("precipitacion_invervalo_mm"),
   "u" = c("velocidad_viento_ms"),
-  "rh" = c("humedad_relativa_porcentaje"),
   "Qsi"= c("SW_incidente_wattm2"),
-  "Qli"= c("LW_incidente_wattm2")
+  "Qli"= c("LW_incidente_wattm2"),
+  "rh" = c("humedad_relativa_porcentaje")
 )
 
 CRHM_var_OBS_num = list(
   "t" = 48,
   "p" = 1,
   "u" = 1,
-  "rh" = 48,
   "Qsi"= 48,
-  "Qli"= 48
+  "Qli"= 48,
+  "rh" = 48
 )
 
 for (var_CRHM in names(CRHM_var_OBS_col)) {
   
 
 df = create_obs_file_per_variable(
+  date_init = "2022-03-01",
   meteo_CRHM_variable = var_CRHM,
   obs_matrix = obs[,c("datetime",rep(CRHM_var_OBS_col[[var_CRHM]],
                                      CRHM_var_OBS_num[[var_CRHM]]))]
