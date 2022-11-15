0b71b97f10afe5c56b115a593c3f6d969b9940ca
Francisco Jara <fcojara506@gmail.com>
2022-11-15 19:26:03 -0300

    new obs order in prj file

--- CRHM_model/utils/set_modules_parameters_CRHM.R
+++ CRHM_model/utils/set_modules_parameters_CRHM.R
@@ -1,10 +1,16 @@
 library(CRHMr)
 prj_filename_in = "modelo_crhm_glaciar_echaurren.prj"
 
-modules = CRHMr::readPrjModuleNames(prjFile = prj_filename_in)
 
-a=CRHMr::readPrjParameters(prjFile = prj_filename_in,paramName = "hru_area")
+
+a=CRHMr::readPrjParameters(prjFile = prj_filename_in,
+                           paramName = "HRU_OBS")
+obs_order = c(seq(1,48),
+        rep(1,48),
+        rep(1,48),
+        seq(1,48),
+        seq(1,48))
+
 CRHMr::setPrjParameters(inputPrjFile = prj_filename_in,
-                        paramName = "hru_area",
-                        paramVals = a*2,
-                        outputPrjFile = "parameters_echaurren.par")
+                        paramName = "HRU_OBS",
+                        paramVals = obs_order)
