989f7104532f4ddc868a11b2e7aed1ee6cab27fc
Francisco Jara <fcojara506@gmail.com>
2022-11-18 14:43:10 -0300

    reduce temperature in 5 °C

--- CRHM_model/utils/set_modules_parameters_CRHM.R
+++ CRHM_model/utils/set_modules_parameters_CRHM.R
@@ -51,7 +51,8 @@ par_name_value = list(
   "tmax_allsnow" = 2.5, #default 0
   "tmax_allrain" = 4,#default 4
   "lapse_rate" = 0, #already defined
-  "ClimChng_precip" = 10, #default 1
+  "ClimChng_precip" = 10, #default 1 #########
+  "ClimChng_t" = -5,#default 0 ##########
   ##----------------------------------------------------------------
   ##                            global                             -
   ##----------------------------------------------------------------
