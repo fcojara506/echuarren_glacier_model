4d0f6102eb6e0d0ee9016ddbcc5e0d803574ffc1
Francisco Jara <fcojara506@gmail.com>
2022-11-23 16:34:47 -0300

    Create plot_obs.R

---
+++ CRHM_model/plot_obs.R
@@ -0,0 +1,28 @@
+rm(list = ls())
+library(dplyr)
+library(data.table)
+
+basin_df = read.csv2(file = "basin_data/HRU_basin_properties.csv")
+
+observaciones =
+  list.files(
+  path = "CRHM_model_output/archivos/",
+  pattern = "obs_",
+  full.names = T)
+
+a=lapply(observaciones , readRDS) %>% rbindlist() %>% 
+  mutate(HRU = as.integer(HRU)) %>% 
+  merge(basin_df,by = "HRU")
+
+library(ggplot2)
+library(scales)
+ggplot(data = a)+
+  geom_line(aes(x = datetime,
+                y = value,
+                col = elevacion_msnm,
+                group= elevacion_msnm ))+
+  scale_x_datetime(labels = date_format("%b"),
+                   date_breaks = "1 months")+
+  facet_wrap(~var,scales = "free_y")+
+  labs(x = "fecha (2022)",
+       col = "elevación (m)")
