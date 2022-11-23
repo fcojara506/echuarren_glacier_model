1da5a187fdcdd87caa40bc91f17452a1c8285991
Francisco Jara <fcojara506@gmail.com>
2022-11-23 16:34:31 -0300

    save obs from crhm

--- CRHM_model/utils/run_export_result_CRHM.R
+++ CRHM_model/utils/run_export_result_CRHM.R
@@ -2,22 +2,65 @@ rm(list = ls())
 source("base/run_CRHM_model.R")
 
 
+
+
+export_obs <- function(variable_modulo,modulo = "obs") {
+  
 prj_filename = "modelo_crhm_glaciar_echaurren_v2.prj"
 # run
-setPrjDates(inputPrjFile = prj_filename,
-            startDate = '2022 3 1',
-            endDate = '2022 11 1')  
+setPrjDates(inputPrjFile = prj_filename,startDate = '2022 3 1', endDate = '2022 11 1')  
 
+output_name = paste(modulo,variable_modulo,sep="_")
 set_output_variables(prj_filename = prj_filename,
-                     new_variable = c("SnobalCRHM","SWE",as.character(seq(1,48))))
-run_CRHM_output(output_filename = "snobal_SWE.txt")
+                     new_variable = c(modulo,variable_modulo,as.character(seq(1,48))))
+run_CRHM_output(output_filename = paste0(output_name,".txt"))
 
-a=CRHMr::readOutputFile(outputFile = "CRHM_model_output/snobal_SWE.txt",
+a=CRHMr::readOutputFile(outputFile = paste0("CRHM_model_output/",output_name,".txt"),
                         timezone = 'etc/GMT+4')
+library(tidyr)
+#b = head(a,-24)
 
-b = head(a,-24)
-c = reshape2::melt(b,id.vars = "datetime")
+c = reshape2::melt(a,id.vars = "datetime") %>% 
+  separate(variable,c("var","HRU"),
+           sep = paste0(variable_modulo,".")) %>% 
+  mutate(var = variable_modulo)
 
-library(ggplot2)
-ggplot(data = c)+
-  geom_line(aes(x = datetime, y = value, col = variable))
+saveRDS(object = c,
+        file = paste0("CRHM_model_output/archivos/",output_name,".RDS")
+        )
+return(c)
+}
+
+variables = c("hru_t",
+              "hru_ea",
+              "hru_p",
+              "hru_snow",
+              "hru_rain",
+              "hru_rh",
+              "hru_u")
+
+sapply(variables, export_obs)
+
+#label_y= "temperatura del aire (°C)"
+
+#basin_df = read.csv2(file = "basin_data/HRU_basin_properties.csv")
+#d = merge(c,basin_df,by = "HRU")
+
+# library(ggplot2)
+# library(scales)
+# ggplot(data = d)+
+#   geom_line(aes(x = datetime,
+#                 y = value,
+#                 col = elevacion_msnm,
+#                 group= elevacion_msnm ))+
+#   scale_x_datetime(labels = date_format("%b"),
+#                    date_breaks = "1 months")+
+#   labs(x = "fecha (2022)",
+#        y = label_y,
+#        col = "elevación (m)")
+# 
+# ggsave(filename = paste0("CRHM_model_output/figuras/",output_name,".png"),
+#        width = 7,
+#        height = 2,
+#        dpi = 400)
+
