569804317d05212c7b469848e659f22ec7d3567e
Francisco Jara <fcojara506@gmail.com>
2022-11-23 14:11:42 -0300

    Update run_CRHM_model.R

--- CRHM_model/base/run_CRHM_model.R
+++ CRHM_model/base/run_CRHM_model.R
@@ -1,27 +1,63 @@
 library(CRHMr)
 
 
+empty_output_variable <- function(prj_filename = "modelo_crhm_glaciar_echaurren_v2.prj") {
+  CRHMr::setPrjOutputVariables(prj_filename, 'null')
+}
+set_output_variables <- function(prj_filename = "modelo_crhm_glaciar_echaurren_v2.prj",
+                                 new_variable) {
 
-crhm_path = "/Users/fco/Documents/GitHub/crhmcode/crhmcode/build/crhm"
-prj_filename = "modelo_crhm_glaciar_echaurren.prj"
+df = df <- data.frame(matrix(ncol = 3, nrow = 0))
+names(df) = c("module","variable","HRUs") 
+
+success = CRHMr::setPrjOutputVariables(inputPrjFile = prj_filename,
+                             variables = new_variable)
+return(success)
+
+}
+
+get_output_variables <- function(prj_filename = "modelo_crhm_glaciar_echaurren_v2.prj") {
+df = CRHMr::readPrjOutputVariables(prjFile = prj_filename,
+                                   asDataframe = T)
+return(df)
+  
+}
+
+add_output_variable <- function(prj_filename = "modelo_crhm_glaciar_echaurren_v2.prj",
+                                new_variable) {
+  df = get_output_variables(prj_filename = prj_filename)
+  df2 = rbind(df,new_variable)
+  success = set_output_variables(prj_filename = prj_filename,new_variable = df2)
+  return(success)
+}
+
+run_CRHM_output <- function(
+crhm_path = "/Users/fco/Documents/GitHub/crhmcode/crhmcode/build/crhm",
+prj_filename = "modelo_crhm_glaciar_echaurren_v2.prj",
 obs_filenames = c(
     "meteo_data/CRHM_obs_data/obs_test_u.obs",
     "meteo_data/CRHM_obs_data/obs_test_t.obs",
     "meteo_data/CRHM_obs_data/obs_test_rh.obs",
     "meteo_data/CRHM_obs_data/obs_test_Qsi.obs",
     "meteo_data/CRHM_obs_data/obs_test_Qli.obs",
-    "meteo_data/CRHM_obs_data/obs_test_p.obs",
-    "meteo_data/CRHM_obs_data/obs_test_h_snow.obs")
+    "meteo_data/CRHM_obs_data/obs_test_p.obs"
+    ),
+output_filename = "output.txt") {
+
   
     #automate
-    automatePrj(prj_filename,quiet = F,logfile = "")
+    automatePrj(prj_filename)
+    
+    # set observation list
     setPrjObs(prj_filename,
               obsFiles = obs_filenames)
     
     runCRHM(CRHMfile = crhm_path ,
-            prjFile = prj_filename
+            prjFile = prj_filename,
+            outFile = paste0("CRHM_model_output/",output_filename)
             )
-            #logfile = "",
-            #outFile = "CRHM_model_output/output.txt",
-
+file.remove("CRHMr.log")
+file.remove("crhmRun.log")
+return(T)
+}
 
