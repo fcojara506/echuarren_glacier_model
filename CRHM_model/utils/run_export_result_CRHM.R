9aabe2113b3f94e0ab7b6cee98dc26c4a61d40bb
Francisco Jara <fcojara506@gmail.com>
2022-11-23 14:11:26 -0300

    new output

--- CRHM_model/utils/run_export_result_CRHM.R
+++ CRHM_model/utils/run_export_result_CRHM.R
@@ -1,5 +1,14 @@
 rm(list = ls())
 source("base/run_CRHM_model.R")
 
-# run 
-run_CRHM_output()
+
+prj_filename = "modelo_crhm_glaciar_echaurren_v2.prj"
+# run
+
+set_output_variables(prj_filename = prj_filename,
+                     new_variable = c("SnobalCRHM","SWE",as.character(seq(1,48))))
+run_CRHM_output(output_filename = "snobal_SWE.txt")
+
+source("base/read_output_file.R")
+CRHMr::readExportFile(exportFile = "CRHM_model_output/snobal_SWE.txt",
+                        timezone = 'etc/GMT+4')
