311b3ec01a75cd978417f7fbb5ecf945185754e9
Francisco Jara <fcojara506@gmail.com>
2022-11-28 17:59:23 -0300

    new way of water balance

---
+++ CRHM_model/utils/mass_balance_alternative_version.R
@@ -0,0 +1,82 @@
+library(CRHMr)
+library(data.table)
+library(dplyr)
+
+rm(list = ls())
+
+
+list_index <- function(lista) {sapply(lista, function(x) grep(paste0("^",x), df_units$time,fixed = F))}
+
+sep_col <- function(df) {
+  library(data.table)
+  library(dplyr)
+  
+  df = data.table(df) %>% 
+    melt.data.table(id.vars = "datetime") %>%
+    .[, c("var", "HRU") := tstrsplit(variable, ".", fixed=TRUE)] %>% 
+    .[, HRU:= as.integer(HRU)] %>% 
+    .[, value:= as.double(value)] %>% 
+    select(-variable)
+  
+  return(df)
+}
+
+areal_avg <- function(df) {
+  
+  df = data.table(df) %>% 
+  merge.data.table(basin_df,by="HRU") %>% 
+    mutate(value_areaavg = value * area_km2/sum(area_km2)) %>% 
+    select(datetime,var,value_areaavg,value) %>% 
+    group_by(datetime,var) %>% 
+    summarise(var_hour=sum(value_areaavg))
+  
+  return(df)
+}
+date_YM <- function(df) {
+  ## hourly to monthly
+  df$date = df$datetime %>% format("%Y-%m-01") %>% as.Date()
+  return(df)
+  
+}
+
+basin_df = read.csv2(file = "basin_data/HRU_basin_properties.csv")[,c("HRU","area_km2")]
+
+
+
+df_filename = "CRHM_output_2.txt"
+df = readOutputFile(df_filename,timezone = "etc/GMT+4")
+df_units =CRHMr::readOutputUnits(df_filename)
+
+flujos_hru_index = c("hru_rain","hru_snow","hru_actet","hru_subl","intcp_evap","Subl_Cpy","E_s_int") %>% list_index
+almacen_hru_index = c("firn","ice","gw","soil_moist","Sd") %>% list_index
+caudales_cuenca_index = c("basinflow","basingw")%>% list_index
+snowloss_cuenca_index = c("BasinSnowLoss")%>% list_index
+
+##### from HRU to URH
+## mm/int
+cuenca_flujos = df[,c(1,flujos_hru_index)] %>%
+  sep_col %>% 
+  areal_avg %>% 
+  date_YM
+
+## mm
+cuenca_almacen = df[,c(1,almacen_hru_index)]%>%
+  sep_col %>%
+  areal_avg%>% 
+  date_YM
+
+## m3/int to mm/int
+cuenca_caudales = df[,c(1,caudales_cuenca_index)]%>%
+  sep_col %>% 
+  mutate(var_hour = value*1e9/(1e12*sum(basin_df$area_km2))) %>% 
+  select(datetime,var,var_hour)%>% 
+  date_YM
+
+## mm/int
+cuenca_snowloss = df[,c(1,snowloss_cuenca_index)]%>% sep_col%>% 
+  date_YM
+
+rm(df)
+gc()
+
+
