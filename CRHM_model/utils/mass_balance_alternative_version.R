993dba50db88f94e471726041a86898f4a096704
Francisco Jara <fcojara506@gmail.com>
2022-11-29 18:39:07 -0300

    Update mass_balance_alternative_version.R

--- CRHM_model/utils/mass_balance_alternative_version.R
+++ CRHM_model/utils/mass_balance_alternative_version.R
@@ -22,10 +22,11 @@ sep_col <- function(df) {
 }
 
 areal_avg <- function(df) {
+  area_basin = sum(basin_df$area_km2)
   
   df = data.table(df) %>% 
   merge.data.table(basin_df,by="HRU") %>% 
-    mutate(value_areaavg = value * area_km2/sum(area_km2)) %>% 
+    mutate(value_areaavg = value * area_km2/area_basin) %>% 
     select(datetime,var,value_areaavg,value) %>% 
     group_by(datetime,var) %>% 
     summarise(var_hour=sum(value_areaavg))
@@ -54,29 +55,54 @@ snowloss_cuenca_index = c("BasinSnowLoss")%>% list_index
 
 ##### from HRU to URH
 ## mm/int
-cuenca_flujos = df[,c(1,flujos_hru_index)] %>%
+cuenca_flujos = df[,c(1,flujos_hru_index)] %>% 
   sep_col %>% 
   areal_avg %>% 
-  date_YM
+  #date_YM %>% 
+  group_by(var) %>% 
+  summarise(var_total=sum(var_hour))
 
 ## mm
 cuenca_almacen = df[,c(1,almacen_hru_index)]%>%
   sep_col %>%
   areal_avg%>% 
-  date_YM
+  #date_YM%>% 
+  group_by(var) %>% 
+  summarise(var_total=first(var_hour)-last(var_hour))
 
 ## m3/int to mm/int
 cuenca_caudales = df[,c(1,caudales_cuenca_index)]%>%
   sep_col %>% 
   mutate(var_hour = value*1e9/(1e12*sum(basin_df$area_km2))) %>% 
   select(datetime,var,var_hour)%>% 
-  date_YM
+  #date_YM%>% 
+  group_by(var) %>% 
+  summarise(var_total=sum(var_hour))
 
 ## mm/int
-cuenca_snowloss = df[,c(1,snowloss_cuenca_index)]%>% sep_col%>% 
-  date_YM
+cuenca_snowloss = df[,c(1,snowloss_cuenca_index)] %>%
+  sep_col%>% 
+  #date_YM %>% 
+  select(-HRU) %>% 
+  rename(var_hour = value)%>% 
+  group_by(var) %>% 
+  summarise(var_total=sum(var_hour))
 
-rm(df)
-gc()
+var_in= c("hru_rain","hru_snow","firn","gw","ice","Sd","soil_moist")
+
+df2 = rbind(cuenca_almacen,
+            cuenca_caudales,
+            cuenca_flujos,
+            cuenca_snowloss) %>% 
+  mutate(in_or_out = ifelse(var %in% var_in, "in","out")) %>% 
+  mutate(var_total = ifelse(var %in% c("E_s_int"), -var_total,var_total))
+
+
+df_inout = df2 %>%
+  group_by(in_or_out) %>% 
+  summarise(var_sum = sum(var_total))
+  
+  
+
 
 
